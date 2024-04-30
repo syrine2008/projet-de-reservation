90178dd215464efd300bb3bfc974db7b39af507b
syrine2008 <zaffati.sirine@gmail.com>
2024-04-30 21:13:41 +0200

    first commit

---
+++ projet-reservation/src/main/java/com/example/demo/ProjetReservationApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetReservationApplication.class, args);
+	}
+
+}
